3401d7a7f5e066fb0a1d4beaabb566f306622cd2
Vickywu886886 <vickywy660668@gmail.com>
2023-11-03 16:58:41 -0700

    added icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import "./Navbar.css";
 import logoImg from "../../images/logo.png";
 import {HiOutlineMenuAlt3} from "react-icons/hi";
+import {  AiOutlineHeart } from 'react-icons/ai';
+import {VscAccount} from 'react-icons/vsc';
+import {AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineQuestionCircle} from 'react-icons/ai';
+
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -26,16 +31,17 @@ const Navbar = () => {
         <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
           <ul className = "navbar-nav">
             <li className='nav-item'>
-              <Link to = "/book" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>Home</Link>
+              <Link to = "/book" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'><AiOutlineHome style={{position:"relative",top:"2px", marginRight:"5px"}}/>Home</Link>
             </li>
             <li className='nav-item'>
-              <Link to = "/about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>about</Link>
+              <Link to = "/about" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'><AiOutlineQuestionCircle style={{position:"relative",top:"2px", marginRight:"5px"}}/>about</Link>
             </li>
             <li className='nav-item'>
-              <Link to = "/account" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>account</Link>
+              <Link to = "/account" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'><VscAccount  style={{position:"relative",top:"2px", marginRight:"5px"}}/>account</Link>
             </li>
-            <li className='nav-item'>
-              <Link to = "/wishlist" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'>wishlist</Link>
+            <li className='nav-item'  style={{display:"flex", alignItems:"center"}}>
+              <Link to = "/wishlist" className='nav-link text-uppercase text-white fs-22 fw-6 ls-1'><AiOutlineHeart style={{position:"relative",top:"2px", marginRight:"5px"}} />wishlist
+              </Link>
             </li>
           </ul>
         </div>
--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Wishlist.css"
+import {  AiOutlineHeart } from 'react-icons/ai';
+
 
 export default function Wishlist(){
     return(
         <div>
         <Navbar/>
         <div className="wishlist-page">
-        <h1>Wishlist</h1>
+        <h1>Wishlist<AiOutlineHeart/></h1>
         </div>
         </div>
     )
